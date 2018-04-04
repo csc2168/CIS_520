17029bf7cbd6db62aa47a84e7cd0cb35f71ae354
Sandeep Dcunha <dcunhas@users.noreply.github.com>
2018-04-03 22:32:53 -0400

    Dataset preprocessing
    Create ground truth images and convert images to png

---
+++ dataset_preprocess.py
@@ -0,0 +1,29 @@
+import os
+from PIL import Image
+import numpy as np
+import csv
+
+os.chdir('FullIJCNN2013')
+
+if not os.path.exists("png_images"):
+    os.mkdir("png_images")
+if not os.path.exists("gt_images"):
+    os.mkdir("gt_images")
+classes = []
+with open("gt.txt") as csv_file:
+    gt = csv.reader(csv_file, delimiter=';')
+    for row in gt:
+        im_name, x1, y1, x2, y2, c = row
+        x1 = int(x1)
+        y1 = int(y1)
+        x2 = int(x2)
+        y2 = int(y2)
+        basename = os.path.splitext(im_name)[0]
+        classes.append(int(c))
+        im = Image.open(im_name)
+        gt_im = np.full(np.array(im).shape[:2], 43, dtype=np.uint8) #Set background to max class + 1
+        gt_im[y1:y2+1, x1:x2+1] = c
+        gt_im = Image.fromarray(gt_im)
+        
+        im.save(os.path.join("png_images", basename + ".png"))
+        gt_im.save(os.path.join("gt_images", basename + ".png"))
