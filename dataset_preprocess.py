e5f525f0e0c042ab0229d82a90d5c0e8249137db
Sandeep Dcunha <sdcunha@umass.edu>
2018-04-22 22:12:10 -0400

    Now modify rather than overwrite if there are multiple lines for labels for an image.

--- dataset_preprocess.py
+++ dataset_preprocess.py
@@ -5,10 +5,12 @@ import csv
 
 os.chdir('../FullIJCNN2013')
 
-if not os.path.exists("images"):
-    os.mkdir("images")
-if not os.path.exists("annotations"):
-    os.mkdir("annotations")
+image_dir = "images"
+annotations_dir = "annotations"
+if not os.path.exists(image_dir):
+    os.mkdir(image_dir)
+if not os.path.exists(annotations_dir):
+    os.mkdir(annotations_dir)
 classes = []
 with open("gt.txt") as csv_file:
     gt = csv.reader(csv_file, delimiter=';')
@@ -21,9 +23,14 @@ with open("gt.txt") as csv_file:
         basename = os.path.splitext(im_name)[0]
         classes.append(int(c))
         im = Image.open(im_name)
-        gt_im = np.full(np.array(im).shape[:2], 43, dtype=np.uint8) #Set background to max class + 1
+
+        gt_im_path = os.path.exists(os.path.join(annotations_dir, basename + '.png'))
+        if os.path.exists(gt_im_path):
+            gt_im = np.array(Image.open(gt_im_path))
+        else:
+            gt_im = np.full(np.array(im).shape[:2], 43, dtype=np.uint8) #Set background to max class + 1
         gt_im[y1:y2+1, x1:x2+1] = c
         gt_im = Image.fromarray(gt_im)
         
-        im.save(os.path.join("images", basename + ".png"))
-        gt_im.save(os.path.join("annotations", basename + ".png"))
+        im.save(os.path.join(image_dir, basename + ".png"))
+        gt_im.save(gt_im_path)
