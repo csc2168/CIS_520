51af17e864fc83792c5528edec52a7edfbb2adc3
Connor Callahan <cocal@sas.upenn.edu>
2018-04-24 19:27:09 -0400

    updated CNN algorithm

--- cnn.py
+++ cnn.py
@@ -1,17 +1,3 @@
-# Convolutional Neural Network
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# pip install tensorflow
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Building the CNN
-
-# Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
@@ -19,38 +5,36 @@ from keras.layers import Flatten
 from keras.layers import Dense
 from keras.layers import Dropout
 
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import array_to_img
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
 
-# Initialising the CNN
-classifier = Sequential()
 
-# Step 1 - Convolution
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), padding = 'same', activation = 'relu'))
+# CNN Initialization
+cnn = Sequential()
 
-# Step 2 - Pooling
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+# 1st convolutional layer & pool
+cnn.add(Conv2D(32, (3, 3), input_shape = (32, 32, 3), padding = 'same', activation = 'relu'))
+cnn.add(MaxPooling2D(pool_size = (2, 2)))
 
-# Adding a second convolutional layer
-classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+# 2nd layer
+cnn.add(Conv2D(64, (3, 3), activation = 'relu'))
+cnn.add(MaxPooling2D(pool_size = (2, 2)))
 
-# Add third convolutional layer
-classifier.add(Conv2D(64, (3, 3), activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+# 3rd layer
+cnn.add(Conv2D(128, (3, 3), activation = 'relu'))
+cnn.add(MaxPooling2D(pool_size = (2, 2)))
 
-# Step 3 - Flattening
-classifier.add(Flatten())
+cnn.add(Flatten())
 
-# Step 4 - Full connection
-classifier.add(Dense(units = 512, activation = 'relu'))
-classifier.add(Dropout(.5))
-classifier.add(Dense(units = 43, activation = 'softmax'))
+# Full connection and dropout
+cnn.add(Dense(units = 1024, activation = 'relu'))
+cnn.add(Dropout(.5))
+cnn.add(Dense(units = 43, activation = 'softmax'))
 
 # Compiling the CNN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
-# Part 2 - Fitting the CNN to the images
-
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, load_img, img_to_array
+cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 train_datagen = ImageDataGenerator(
         width_shift_range=0.2,
@@ -70,17 +54,17 @@ test_datagen = ImageDataGenerator(
         fill_mode='nearest')
 
 training_set = train_datagen.flow_from_directory('/home/csc2168/hw/cis/CIS_520/GTSRB/Final_Training/Images/',
-                                                 target_size = (64, 64),
+                                                 target_size = (32, 32),
                                                  batch_size = 32)
 
-test_set = test_datagen.flow_from_directory('/home/csc2168/hw/cis/CIS_520/GTSRB/Final_Training/val_images/',
-                                            target_size = (64, 64),
+val_set = test_datagen.flow_from_directory('/home/csc2168/hw/cis/CIS_520/GTSRB/Final_Training/val_images/',
+                                            target_size = (32, 32),
                                             batch_size = 32)
 
-classifier.fit_generator(training_set,
-                         steps_per_epoch = 2000,
-                         epochs = 10,
-                         validation_data = test_set,
-validation_steps = 500)
+cnn.fit_generator(training_set,
+                         steps_per_epoch = 10000,
+                         epochs = 6,
+                         validation_data = val_set,
+validation_steps = 2000)
 
-classifier.save_weights('cnn_weights.h5');
+cnn.save_weights('cnn_weights5.h5');
